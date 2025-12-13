eaaa267d9105a67813bbbeb03832575e7f7f0d23
Rafaelsanyoto <rafaelsanyoto2187@gmail.com>
2025-12-13 22:25:46 +0700

    Add files via upload

---
+++ lang/en/messages.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'course_catalogue' => 'Course Catalogue',
+    'new_course'       => 'New Course',
+    'admin'            => 'Admin',
+    'edit'             => 'Edit',
+    'delete'           => 'Delete',
+    'apply'            => 'Apply',
+    'price'            => 'Price',
+    'no_courses'       => 'No courses found.',
+    'confirm_delete'   => 'Are you sure you want to delete this course?',
+];
---
+++ lang/id/messages.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'course_catalogue' => 'Katalog Kursus',
+    'new_course'       => 'Tambah Kursus',
+    'admin'            => 'Admin',
+    'edit'             => 'Ubah',
+    'delete'           => 'Hapus',
+    'apply'            => 'Daftar',
+    'price'            => 'Harga',
+    'no_courses'       => 'Tidak ada kursus ditemukan.',
+    'confirm_delete'   => 'Apakah Anda yakin ingin menghapus kursus ini?',
+];
