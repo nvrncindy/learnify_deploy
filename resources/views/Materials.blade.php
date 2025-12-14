816e3bbcb665457c4ed5a02ebff1aaaaf7ec0a63
Rafaelsanyoto <rafaelsanyoto2187@gmail.com>
2025-12-14 13:37:57 +0700

    Update Materials.blade.php

--- resources/views/Materials.blade.php
+++ resources/views/Materials.blade.php
@@ -27,77 +27,77 @@
                 </div>
             </div>
 
+            <form action="{{ route('course.submit', $course->id) }}" method="POST">
+                @csrf
 
-
-
-            <div class="card mb-4 shadow-sm">
-                <div class="card-body">
-                    <h5 class="card-title fw-bold">Question 1</h5>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                    <ul class="list-unstyled">
-                        <li class="mb-2">
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="q1" id="q1-option1">
-                                <label class="form-check-label" for="q1-option1">
-                                    A
-                                </label>
-                            </div>
-                        </li>
-                        <li class="mb-2">
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="q1" id="q1-option2">
-                                <label class="form-check-label" for="q1-option2">
-                                    B
-                                </label>
-                            </div>
-                        </li>
-                        <li class="mb-2">
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="q1" id="q1-option3">
-                                <label class="form-check-label" for="q1-option3">
-                                    C
-                                </label>
-                            </div>
-                        </li>
-                    </ul>
+                <div class="card mb-4 shadow-sm">
+                    <div class="card-body">
+                        <h5 class="card-title fw-bold">{{ __('messages.question') }} 1</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <ul class="list-unstyled">
+                            <li class="mb-2">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="q1" id="q1-option1" value="A">
+                                    <label class="form-check-label" for="q1-option1">
+                                        A
+                                    </label>
+                                </div>
+                            </li>
+                            <li class="mb-2">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="q1" id="q1-option2" value="B">
+                                    <label class="form-check-label" for="q1-option2">
+                                        B
+                                    </label>
+                                </div>
+                            </li>
+                            <li class="mb-2">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="q1" id="q1-option3" value="C">
+                                    <label class="form-check-label" for="q1-option3">
+                                        C
+                                    </label>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
 
 
-            <div class="card mb-4 shadow-sm">
-                <div class="card-body">
-                    <h5 class="card-title fw-bold">Question 2</h5>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                    <ul class="list-unstyled">
-                        <li class="mb-2">
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="q2" id="q2-option1">
-                                <label class="form-check-label" for="q2-option1">Option 1</label>
-                            </div>
-                        </li>
-                        <li class="mb-2">
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="q2" id="q2-option2">
-                                <label class="form-check-label" for="q2-option2">Option 2</label>
-                            </div>
-                        </li>
-                        <li class="mb-2">
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="q2" id="q2-option3">
-                                <label class="form-check-label" for="q2-option3">Option 3</label>
-                            </div>
-                        </li>
-                    </ul>
+                <div class="card mb-4 shadow-sm">
+                    <div class="card-body">
+                        <h5 class="card-title fw-bold">{{ __('messages.question') }} 2</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <ul class="list-unstyled">
+                            <li class="mb-2">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="q2" id="q2-option1" value="Option 1">
+                                    <label class="form-check-label" for="q2-option1">{{ __('messages.option') }} 1</label>
+                                </div>
+                            </li>
+                            <li class="mb-2">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="q2" id="q2-option2" value="Option 2">
+                                    <label class="form-check-label" for="q2-option2">{{ __('messages.option') }} 2</label>
+                                </div>
+                            </li>
+                            <li class="mb-2">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="q2" id="q2-option3" value="Option 3">
+                                    <label class="form-check-label" for="q2-option3">{{ __('messages.option') }} 3</label>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
 
-            <div class="text-center">
-                <button type="submit" class="btn btn-primary btn-lg rounded-pill px-5">Submit</button>
-            </div>
+                <div class="text-center">
+                    <button type="submit" class="btn btn-primary btn-lg rounded-pill px-5">{{ __('messages.submit_btn') }}</button>
+                </div>
+            </form>
+
         </div>
     </div>
 </div>
 
 @endsection
-
-
